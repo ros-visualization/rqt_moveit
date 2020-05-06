2d6a22c88a5c5e46c9f7a38c1a947f91e3255e5f
Arne Hitzmann <arne.hitzmann@gmail.com>
2020-05-06 17:57:16 +0900

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,41 +6,34 @@ package_name = 'rqt_moveit'
 version = '0.5.8'
 
 setup(
-    name = package_name,
-    version = version,
-    packages = [package_name],
-    package_dir = {'': 'src'},
-    data_files = [
+    name=package_name,
+    version=version,
+    packages=[package_name],
+    package_dir={'': 'src'},
+    data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
         ('share/' + package_name + '/resource',
             ['resource/moveit_top.ui']),
+        ('lib/' + package_name, ['scripts/rqt_moveit'])
     ],
-    install_requires = ['setuptools'],
-    zip_safe = True,
-    author = "Isaac Saito",
-    maintainer = "Isaac Saito, Arne Hitzmann",
-    maintainer_email = "iisaito@kinugarage.com, arne.hitzmann@gmail.com",
-    keywords = ["ROS 2"],
-    classifiers = [
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author="Isaac Saito",
+    maintainer="Isaac Saito, Arne Hitzmann",
+    maintainer_email="iisaito@kinugarage.com, arne.hitzmann@gmail.com",
+    keywords=["ROS 2"],
+    classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description = (
-        'rqt_robot_monitor displays diagnostics_agg topics messages that \
-        are published by "http://www.ros.org/wiki/diagnostic_aggregator" (diagnostic_aggregator). \
-        rqt_robot_monitor is a direct port to rqt of \
-        "http://www.ros.org/wiki/robot_monitor" (robot_monitor).'
+    description=(
+        'An rqt-based tool that assists monitoring tasks for MoveIt2 motion planner developers and users.'
     ),
-    license = 'BSD',
-    tests_require = ['pytest'],
-    entry_points = {
-        'console_scripts': [
-            'rqt_moveit = rqt_moveit.moveit_widget:main'
-        ]
-    }
+    license='BSD',
+    scripts=['scripts/rqt_moveit'],
 )
